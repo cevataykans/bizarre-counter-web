0c70a3240613520355bd63213c5789b6916d9026
cevataykans <aykansevinc66@gmail.com>
2022-08-16 13:46:40 +0300

    Create project

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function Footer() {
+    return (
+        <div className="Footer">
+            <label>This is my label</label>
+        </div>
+    );
+}
+
+export default App;
